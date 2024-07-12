09aac6f6b68773de9d76008de76a9565c395d77c
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-07-12 23:58:44 +0530

    jwt auth token for admin created

--- Week 3/practice/week-3/02-course-app-easy-2/index.js
+++ Week 3/practice/week-3/02-course-app-easy-2/index.js
@@ -1,15 +1,61 @@
 const express = require('express');
 const app = express();
-
+const jwt = require('jsonwebtoken');
 app.use(express.json());
 
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+const adminSecretKey = 'adM1nSecret';
+const userSecretKey = 'Us3RSecret';
+
+const userVerify = (user) => {
+  const payload = user;
+  return jwt.sign(payload, userSecretKey, { expiresIn: '1h' });
+}
+
+const adminJWt = (admin) => {
+  const payload = {"username": admin.username};
+  return jwt.sign(payload, adminSecretKey, { expiresIn: '1h' });
+}
+
+const adminVerify = (req, res, next) => {
+  const auth = req.headers.Authorization;
+  const token = auth.split(" ")[1];
+  jwt.verify(token, adminSecretKey, (err, user) => {
+    if (err) {
+      res.status(403);
+    } else {
+      req.user = user;
+      next();
+    }
+  })
+}
+
+const userJWT = (req, res, next) => {
+  const {username, password} = req.headers;
+  const user = USERS.find((a) => a.username === username && a.password === password);
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(400).send("User authorization failed");
+  }
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const admin = req.body;
+  const adminExists = ADMINS.find((a) => a.username === username);
+  if (adminExists) {
+    res.json("Admin already present");
+  } else {
+    ADMINS.push(admin);
+    let token = adminJWt(admin);
+    res.json({'message': 'User created successfully', 'token': token})
+  }
 });
 
 app.post('/admin/login', (req, res) => {
