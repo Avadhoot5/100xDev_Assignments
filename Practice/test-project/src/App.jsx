77116207a78c1e54f7d9b16a5d565a26a42b7da0
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-09-22 12:26:43 +0530

    3 assignment questions initaited

--- Practice/test-project/src/App.jsx
+++ Practice/test-project/src/App.jsx
@@ -1,19 +1,41 @@
 import './App.css'
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import Counter from './component/Counter'
+
+const nums = new Array(1000000).fill(0).map((_,i) => {
+  return {
+    index: i,
+    favNum: i === 9500000
+  }
+})
 
 function App() {
 
-  const [value, setValue] = useState(true);
+  // const [value, setValue] = useState(true);
+  // const toggleBtn = () => {
+  //   setValue(!value);
+  // }
 
-  const toggleBtn = () => {
-    setValue(!value);
-  }
+  const [count, setCount] = useState(0);
+  const [numbers, setNumbers] = useState(nums);
+  
+  // expensive operation
+  // const favNumber = numbers.find((item) => item.favNum === true);
+
+  const favNumber = useMemo(() => numbers.find((item) => item.favNum === true),[numbers])
 
   return (
     <>
-      <button onClick={toggleBtn}>Toggle</button>
-      {value && <Counter/>}
+      {/* <button onClick={toggleBtn}>Toggle</button>
+      {value && <Counter/>} */}
+      <div>
+        My fav number is: {favNumber?.index}
+      </div>
+      <div>
+        Counter value: {count}
+        
+      </div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   )
 }
