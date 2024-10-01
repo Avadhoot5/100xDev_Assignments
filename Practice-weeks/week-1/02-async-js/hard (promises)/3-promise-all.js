d24ee2f8e036a1ad8ac9bca43f978b0d55dcf50b
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-01 13:53:42 +0530

    promise all assignment implemented

--- Practice-weeks/week-1/02-async-js/hard (promises)/3-promise-all.js
+++ Practice-weeks/week-1/02-async-js/hard (promises)/3-promise-all.js
@@ -6,17 +6,38 @@
 
 
 function waitOneSecond() {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('after 1 second');
+        }, 1000);
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('after 2 second')
+        }, 2000);
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('after 3 second')
+        }, 3000);
+    })
 }
 
 function calculateTime() {
-
+    const startTime = new Date().getTime();
+    const ans = Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()])
+    return ans.then((res) => {
+        const endTime = new Date().getTime();
+        console.log(res);
+        const time = (endTime - startTime)/1000;
+        console.log(time);
+    })
 }
+
+calculateTime();
