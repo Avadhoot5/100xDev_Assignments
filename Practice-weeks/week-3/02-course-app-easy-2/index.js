fee9578c4c4079a7e7840d5cc7b4bff7066402e3
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-06 15:23:25 +0530

    jwt token for user routes

--- Practice-weeks/week-3/02-course-app-easy-2/index.js
+++ Practice-weeks/week-3/02-course-app-easy-2/index.js
@@ -33,6 +33,31 @@ const adminVerify = (req, res, next) => {
   if (!token) return res.status(400).json({message: 'Please provide auth token'});
 }
 
+
+const userVerify = (req, res, next) => {
+  const data = req.headers.authorization;
+  if (!data) {
+    res.status(400).json({message: 'Authorization token missing in header' })
+  }
+
+  const token = data.split(' ')[1];
+  if (token) {
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (user) {
+        const userValid = USERS.find((a) => a.username === user.username);
+        if (userValid) {
+          req.user = user;
+          next();
+        }
+      } else {
+        res.status(400).json({message: 'Authorization failed' })
+      }
+    })
+  }
+  if (!token) return res.status(400).json({message: 'Please provide auth token'});
+}
+
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
@@ -88,22 +113,56 @@ app.get('/admin/courses', adminVerify, (req, res) => {
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+  const { username, password } = req.body;
+  const userExists = USERS.find((a) => a.username === username);
+  if (userExists) {
+    res.status(400).json({message: 'User already present'});
+  } else {
+    USERS.push({username, password, purchasedCourses: []});
+    const token = jwt.sign( {username: username}, SECRET_KEY, { expiresIn: '1h' });
+    res.status(201).json({message: 'User created successfully', token: token});
+  }
 });
 
 app.post('/users/login', (req, res) => {
   // logic to log in user
+  const { username, password } = req.headers;
+  const userValid = USERS.find((a) => a.username === username && a.password === password);
+  if (userValid) {
+    const token = jwt.sign( {username: username}, SECRET_KEY, { expiresIn: '1h' });
+    res.status(200).json({message: 'Logged in successfully', token: token});
+  } else {
+    res.status(401).json({message: 'Invalid credentials'});
+  }
 });
 
-app.get('/users/courses', (req, res) => {
+app.get('/users/courses', userVerify, (req, res) => {
   // logic to list all courses
+  res.status(200).json({courses: COURSES});
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post('/users/courses/:courseId', userVerify, (req, res) => {
   // logic to purchase a course
+  const courseId = parseInt(req.params.courseId);
+  const course = COURSES.findIndex((a) => a.id === courseId);
+  
+  if (course !== -1) {
+    req.user.purchasedCourses.push(COURSES[course]);
+    res.status(200).json({ message: 'Course purchased successfully'});
+  } else {
+    res.status(400).json({ message: 'Course ID not found'});
+  }
+
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get('/users/purchasedCourses', userVerify, (req, res) => {
   // logic to view purchased courses
+  if (req.user.purchasedCourses.length > 0) {
+    res.status(200).json({purchasedCourses: req.user.purchasedCourses});
+  } else {
+    res.status(400).json({message: 'No purchased coursese'});
+  }
+
 });
 
 app.listen(3000, () => {
