a7b036e47a3ed3c48aa25dabb5f84abbae75f715
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-07 11:24:51 +0530

    usestate and useeffect added for todos

--- Practice-weeks/week-4/src/App.jsx
+++ Practice-weeks/week-4/src/App.jsx
@@ -1,6 +1,36 @@
+import { useState, useEffect } from "react";
+
+function useTodos() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/todos", {
+      method: "GET"
+    }).then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+        setTodos(data)
+      })
+    });
+    setInterval(() => {
+      fetch("http://localhost:3000/todos", {
+        method: "GET"
+      }).then((res) => {
+        res.json().then((data) => {
+          console.log(data);
+          setTodos(data)
+        })
+      })
+    }, 2000);
+  }, [])
+
+  return todos;
+}
 
 function App() {
 
+  const todos = useTodos();
+  
   return (
     <>
 
