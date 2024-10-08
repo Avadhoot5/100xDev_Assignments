929feb2a1e9b276a8a6fb386ae4b9284d9b21e6a
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-08 13:31:25 +0530

    week 4.1 todo added

--- Practice-weeks/week-4/src/App.jsx
+++ Practice-weeks/week-4/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function useTodos() {
   const [todos, setTodos] = useState([]);
@@ -28,14 +29,62 @@ function useTodos() {
 }
 
 function App() {
+  // hook 
+  // useEffect only runs once - hence the setTimeout, interval methods run as expected.
+  // useEffect(() => {
+  //   console.log("hello from use effect");
+  // }, [])
+  // 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setTodos([{
+  //       title: "updated walk",
+  //       description: "update walk in morning",
+  //       id: 1
+  //     },{
+  //       title: "updated to gym",
+  //       description: "updated gym in morning",
+  //       id: 2
+  //     }])
+  //   },2000)
+  // },[])
 
   const todos = useTodos();
-  
   return (
-    <>
+    <> 
+      <h2>Hello There!</h2>
+      {todos.map((todo) => {
+        return <Todo title = {todo.title} description = {todo.description}></Todo>
+      })}
 
+      {/* <PersonName firstName = {"Test"} lastName = {"Name"} ></PersonName> */}
     </>
   )
 }
+
+function Todo(props) {
+  return <div>
+    <b>Title:</b> {props.title}
+    <br />
+    <b>Desc:</b> {props.description}
+    <button onClick={() => {
+          fetch("http://localhost:3000/todos/:id", {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            })
+    }}>Delete</button>
+    <br />
+  </div>
+}
+
+// above Todo component is linked to week 2 - todoserver.js file 
+
+// function PersonName(props) {
+//   return <div>
+//     {props.firstName} {props.lastName}
+//   </div>
+// }
 
 export default App
